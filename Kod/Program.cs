aeb57315ce82547872b6afc92f0b4f9a14020c6d
Olga <olgaingvar@gmail.com>
2023-03-04 21:52:12 +0200

    Сначала идет ввод текста пользователем

---
+++ Kod/Program.cs
@@ -0,0 +1,60 @@
+﻿string Prompt(string mes) // вывод текста 
+{
+    Console.WriteLine(mes);
+    return Console.ReadLine();
+}
+
+string[] FillArray(int size) // 
+{
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Prompt($"Write {i} element: ");
+    }
+
+    return array;
+}
+
+int CountWordsWithLessThan3Symbols(string[] array)
+{
+    int count = 0;
+    foreach (string str in array)
+    {
+        if (str.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] CopyOnlyLessThan3Symbols(string[] initialArr)
+{
+    int copySize = CountWordsWithLessThan3Symbols(initialArr);
+    string[] newArr = new string[copySize];
+
+    int count = 0;
+    foreach (string str in initialArr)
+    {
+        if (str.Length <= 3)
+        {
+            newArr[count++] = str;
+        }
+    }
+
+    return newArr;
+}
+
+void ShowArr(string[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int size = Int32.Parse(Prompt("Size of array: "));
+string[] arr = FillArray(size);
+string[] duplicate = CopyOnlyLessThan3Symbols(arr);
+ShowArr(duplicate);
