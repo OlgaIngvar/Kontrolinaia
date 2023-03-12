3ba60a3760925a0de9ad8c11d7c0104f1eeb8d39
OlgaIngvar <120969103+OlgaIngvar@users.noreply.github.com>
2023-03-12 21:21:31 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//объявляется два массива: изначальный и второй такой же длины.
+string[] array1 = new string[5] {"un", "doi", "trei", "patru", "unsprezece"}; 
+string[] array2 = new string[array1.Length];
+// объявляется метод, в котором проверяется условие ( <=3 )
+void SecondArrayWithIF(string[] array1, string[] array2)
+{ // переменная count для поочередного закидывания из первого массива во второй
+    int count = 0;
+    // после присвоения переменная count увеличивается на 1 и возвращается к циклу for, 
+    //в котором i также увеличивается на 1
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
